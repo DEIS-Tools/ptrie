fd04dc9d8cf91d5bcb2d1cf93e8979d8ee4aa8b7
Marius Mikučionis <marius@cs.aau.dk>
2026-05-18 09:28:53 +0200

    Disabled LTO as it is not portable (AppleClang requires llvm tools, clang/Linux requires -fuse-ld=lld)

--- cmake/benchmark.cmake
+++ cmake/benchmark.cmake
@@ -13,7 +13,8 @@ FetchContent_Declare(benchmark
 
 set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Enable testing of the benchmark library.")
 set(BENCHMARK_ENABLE_EXCEPTIONS ON CACHE BOOL "Enable the use of exceptions in the benchmark library.")
-set(BENCHMARK_ENABLE_LTO ON CACHE BOOL "Enable link time optimisation of the benchmark library.") # requires llvm-ar on macOS
+# Link-Time-Optimization (LTO) requires llvm tools on macOS and -fuse-ld=lld on clang/Linux, disabled for simplicity
+set(BENCHMARK_ENABLE_LTO OFF CACHE BOOL "Enable link time optimisation of the benchmark library.")
 set(BENCHMARK_USE_LIBCXX OFF CACHE BOOL "Build and test using libc++ as the standard library.")
 set(BENCHMARK_ENABLE_WERROR OFF CACHE BOOL "Build Release candidates with -Werror.")
 set(BENCHMARK_FORCE_WERROR OFF CACHE BOOL "Build Release candidates with -Werror regardless of compiler issues.")
